47097f4d249074e0d9f7c1af02019c48539d377b
prisley03 <prillysci.laindra@gmail.com>
2024-06-19 00:04:12 +0700

    seeding ewallet
    seeding ewallet

---
+++ lib/model/ewalletmodel.dart
@@ -0,0 +1,19 @@
+class EWallet {
+  final String id;
+  final String name;
+  final String imageUrl;
+
+  EWallet({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'imageUrl': imageUrl,
+    };
+  }
+}
---
+++ lib/services/DatabaseService.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RedeemDatabaseService{
+
+  // final CollectionReference redeemCollection = FirebaseFirestore.instance.collection('redeems');
+  //
+  // Future<void> updateUserData(String userId, int PointsRedeemed, double Amount, DateTime RedemptionDate){
+  //
+  // }
+
+
+}
