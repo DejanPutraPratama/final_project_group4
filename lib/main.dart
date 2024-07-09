19bdf312609e03e40524420764f8a1da6cca3485
abihanafi23 <hanafi.abi50@gmail.com>
2024-07-09 13:41:26 +0700

    APP IMPROVEMENT

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:final_project_group4/page/login.dart';
+import 'package:final_project_group4/page/splashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:final_project_group4/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -24,9 +24,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
           useMaterial3: true,
           fontFamily: GoogleFonts.poppins().fontFamily),
-      home: LoginScreen(
-        hasLogOut: false,
-      ),
+      home: const SplashScreen(),
     );
   }
 }
